3c18f491d69c954c1d9d1fd4bc8249c37b2324b9
Dan Lidral-Porter <dlp@aperiodic.org>
2012-09-15 20:20:32 -0700

    Scraping all the images!

--- src/ogle/core.clj
+++ src/ogle/core.clj
@@ -1,89 +1,226 @@
 (ns ogle.core
-  (:require [clj-time.core :as time]
+  (:require [clojure.string :as str]
+            [clj-time.core :as time]
             [clj-time.coerce :as cvt-time]
             [clj-time.format :as fmt-time]
-            [net.cgrand.enlive-html :as html])
-  (:use [clojure.string :only [lower-case trim]]))
+            [net.cgrand.enlive-html :as html]))
   (def % partial)
+
+;;
+;; Attributes
+;;
+
+(def image-attributes
+  #{:w :h :id :url
+    :cam :delay :released :sol :taken-martian :taken-utc :thumbnail :type})
+
+;;
+;; Translations
+;;
+
+(defn label->cell-type
+  [label]
+  (-> label (str/split #" ") first keyword))
+
+(def cam-abbrev->name {"ML" "MastCam Left"
+                       "MR" "MastCam Right"
+                       "NLA" "NavCam Left-A"
+                       "NLB" "NavCam Left-B"
+                       "NRA" "NavCam Right-A"
+                       "NRB" "NavCam Right-B"
+                       "CR" "ChemCam"
+                       "MH" "MAHLI"
+                       "MD" "MARDI"
+                       "FLA" "HazCam Front-Left-A"
+                       "FLB" "HazCam Front-Left-B"
+                       "FRA" "HazCam Front-Right-A"
+                       "FRB" "HazCam Front-Right-B"
+                       "RLA" "HazCam Rear-Left-A"
+                       "RLB" "HazCam Rear-Left-B"
+                       "RRA" "HazCam Rear-Right-A"
+                       "RRB" "HazCam Rear-Right-B"})
+
+(def type-abbrev->type
+  (let [known-types {"B" "subframe"
+                     "C" "subframe"
+                     "D" "cropped"
+                     "E" "fullsize"
+                     "F" "fullsize"
+                     "I" "thumbnail"
+                     "T" "thumbnail"}]
+    (fn [abbrev] (get known-types abbrev abbrev))))
+
+;;
+;; Scrapin'
+;;
+
+(def cell->int (comp #(Integer/parseInt %) html/text))
+
+(def rfc-printer (% fmt-time/unparse (fmt-time/formatters :rfc822)))
+
+(def utc-format (fmt-time/formatter "YYYY MMM dd HH:mm:ss"))
+(def utc-parser (comp (% fmt-time/parse utc-format) str/lower-case))
+(defn cell->utc-date-str
+  [cell]
+  (-> cell
+    html/text str/trim utc-parser rfc-printer))
+
+(def marstime-format (fmt-time/formatter "hh:mm:ss a"))
+(def marstime-parser (% fmt-time/parse marstime-format))
+(defn cell->marstime-str
+  [cell]
+  (-> cell
+    html/text str/trim (str/replace #"\." "") marstime-parser rfc-printer))
+
+(defmulti cell->map
+  (fn [label _] (label->cell-type label)))
+
+(defmethod cell->map :camera
+  [_ cell]
+  {:cam (-> (html/text cell) str/trim cam-abbrev->name)})
+
+(defmethod cell->map :delay
+  [_ cell]
+  {:delay (cell->int cell)})
+
+(defmethod cell->map :height
+  [_ cell]
+  {:h (cell->int cell)})
+
+(defmethod cell->map :lag
+  [_ _]
+  {})
+
+(defmethod cell->map :lmst
+  [_ cell]
+  {:taken-marstime (cell->marstime-str cell)})
+
+(defmethod cell->map :name
+  [_ cell]
+  {:id (-> (html/text cell) str/trim)
+   :url (-> (html/select cell [:a])
+          first
+          (get-in [:attrs :href]))})
+
+(defmethod cell->map :released
+  [_ cell]
+  {:released (cell->utc-date-str cell)})
+
+(defmethod cell->map :sol
+  [_ cell]
+  {:sol (cell->int cell)})
+
+(defmethod cell->map :taken
+  [_ cell]
+  {:taken-utc (cell->utc-date-str cell)})
+
+(defmethod cell->map :thumbnail
+  [_ cell]
+  {:thumbnail-url (-> (html/select cell [:img])
+                    first
+                    (get-in [:attrs :src]))})
+
+(defmethod cell->map :type
+  [_ cell]
+  {:type (-> (html/text cell) str/trim type-abbrev->type)})
+
+(defmethod cell->map :width
+  [_ cell]
+  {:w (cell->int cell)})
+
+(defn row->url
+  [row]
+  (let [img-link (-> (html/select row [:a]) first)]
+    (get-in img-link [:attrs :href])))
+
+(defn row->map
+  [labels row]
+  (let [cell-data (->> (html/select row [:td])
+                    (map cell->map labels)
+                    (apply merge))
+        row-data {:url (row->url row)}]
+    (merge cell-data row-data)))
+
+(defn body->images
+  [body-node]
+  (let [img-table (-> (html/select body-node [:table]) last)
+        [label-row & img-rows] (html/select img-table [:tr])
+        labels (->> (html/select label-row [:th])
+                 (map (comp str/trim html/text)))]
+    (map (% row->map labels) img-rows)))
+
+;;
+;; Fetchin'
+;;
 
 (defn page-url
   ([] (page-url 1))
   ([i] (str "http://curiositymsl.com/?page=" i "&limit=100")))
 
-(def first-page (html/html-resource (java.net.URL. (page-url 1))))
+(defn fetch-page
+  [i]
+  (html/html-resource (java.net.URL. (page-url i))))
 
-(def label->attr {"camera" :cam
-                  "height" :h
-                  "lag (hours)" :delay
-                  "lmst" :taken-martian
-                  "name" :id
-                  "released (UTC)" :released
-                  "sol" :sol
-                  "taken (UTC)" :taken-utc
-                  "thumbnail" nil
-                  "type" :type
-                  "width" :w})
+(defn fetch-images-since
+  [last-id]
+  (let [last-img? (fn [img] (= (:id img) last-id))]
+    (println "fetching page 1")
+    (loop [i 1, imgs ()]
+      (let [page-imgs (body->images (fetch-page i))
+            last-page? (some last-img? page-imgs)
+            imgs' (concat imgs (take-while (complement last-img?) page-imgs))]
+        (if (or last-page? (empty? page-imgs))
+          imgs'
+          (do
+            (Thread/sleep 500) ; ratelimit ourselves
+            (println "fetching page" (inc i))
+            (recur (inc i) imgs')))))))
 
-(def cam-abbrev->name {"ML" "MastCam Left"})
+;;
+;; State & Main
+;;
 
-(def cell->int (comp #(Integer/parseInt %) html/text))
+(def $last-id-file "cache/last-id")
 
-(def utc-format (fmt-time/formatter "YYYY MMM dd HH:mm:ss"))
-(def utc-parser (comp (% fmt-time/parse utc-format) lower-case))
-(def cell->utc-date (comp utc-parser trim html/text))
+(defn last-id
+  "Read the last id we know about from a file in the current directory"
+  []
+  (try (-> (slurp $last-id-file) read-string)
+    (catch java.io.FileNotFoundException _ nil)))
 
-(def marstime-format (fmt-time/formatter "hh:mm:ss a"))
-(def marstime-parser (% fmt-time/parse marstime-format))
-(def cell->mars-time (comp marstime-parser trim html/text))
+(defn set-last-id!
+  [id]
+  (spit $last-id-file (prn-str id)))
 
-(defmulti cell->data
-  (fn [label _] label))
+(def $images-file "cache/images")
 
-(defmethod cell->data :cam
-  [_ cell]
-  (html/text cell))
+(defn old-images
+  "Read the images we already have from a file in the current directory"
+  []
+  (try (-> (slurp $images-file) read-string)
+    (catch java.io.FileNotFoundException _ )))
 
-(defmethod cell->data :delay
-  [_ cell]
-  (cell->int cell))
+(defn dump-all-images!
+  [images]
+  (spit $images-file (prn-str images)))
 
-(defmethod cell->data :h
-  [_ cell]
-  (cell->int cell))
+(defn print-summary
+  [new-images all-images]
+  (println
+    "fetched" (count new-images) "new images, have" (count all-images) "in"
+    "total"))
 
-(defmethod cell->data :id
-  [_ cell]
-  (html/text cell))
-
-(defmethod cell->data :released
-  [_ cell]
-  (cell->utc-date cell))
-
-(defmethod cell->data :sol
-  [_ cell]
-  (cell->int cell))
-
-(defmethod cell->data :taken-martian
-  [_ cell]
-  (cell->mars-time cell))
-
-(defmethod cell->data :taken-utc
-  [_ cell]
-  (cell->utc-date cell))
-
-
-(let [tables (html/select first-page [:table])
-      img-table (last tables)
-      [labels & rows] (html/select img-table [:tr])
-      labels (->> (html/select labels [:th])
-               (map (comp trim html/text))
-               (replace label->attr)
-               (keep identity))
-      first-row (first rows)
-      row-cells #(html/select % [:td])]
-  (row-cells first-row))
+(defn fetch-new-images!
+  []
+  (let [new-imgs (fetch-images-since (last-id))
+        new-id (-> new-imgs first :id)
+        old-imgs (old-images)
+        all-imgs (concat new-imgs old-imgs)]
+    (dump-all-images! all-imgs)
+    (set-last-id! new-id)
+    (print-summary new-imgs all-imgs)))
 
 (defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+  "Fetch new images and add them to the list of known images"
+  [& _]
+  (fetch-new-images!))
