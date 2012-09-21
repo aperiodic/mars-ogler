5c6358816a59541e986f36982106545ae85fcc00
Dan Lidral-Porter <dlp@aperiodic.org>
2012-09-21 03:49:44 -0700

    Update compojure, add ring deps

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]
-                 [compojure "1.1.1"]
+                 [compojure "1.1.3"]
+                 [enlive "1.0.1"]
                  [hiccup "1.0.0"]
-                 [enlive "1.0.1"]]
+                 [ring/ring-core "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.5"]]
   :plugins [[lein-ring "0.7.1"]]
   :ring {:handler mars-ogler.routes/handler})
